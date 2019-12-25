8a410f0343799e093cfcd3a2b1b591d43fa2b0cf
Ravshan <ravkart@gmail.com>
2019-12-25 13:12:44 -0800

    added caporal

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
 
-chokidar
-  .watch('.')
-  .on('add', () => console.log('file added'))
-  .on('change', () => console.log('file changed'));
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of file to execute')
+  .action(args => {
+    const start = debounce(() => {
+      console.log('STARTING USER PROGRAM');
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', () => console.log('file added'))
+      .on('change', () => console.log('file changed'));
+  });
+
+program.parse(process.env);
